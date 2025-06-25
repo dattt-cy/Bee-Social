2e7c8478e16a41b78f1262d04a4374b657d7d020
dattt-cy <victomblack1602@gmail.com>
2025-06-25 17:46:53 +0700

    chore: add remaining config and shared logic files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextTranslate = require('next-translate-plugin')
+const { withNextVideo } = require('next-video/process')
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  },
+  ...nextTranslate(),
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true
+      }
+    ]
+  },
+  env: {
+    NEXT_APP_BEEGIN_DOMAIN: 'https://beegin.onrender.com',
+    // NEXT_APP_BEEGIN_DOMAIN: 'http://localhost:8000',
+    NEXTAUTH_URL: 'http://localhost:3000'
+  },
+  images: {
+    domains: ['images.pexels.com', 'res.cloudinary.com']
+  }
+}
 
-export default nextConfig;
+module.exports = withNextVideo(nextConfig, {
+  provider: 'cloudinary',
+  providerConfig: {
+    cloudinary: { endpoint: 'https://res.cloudinary.com' }
+  }
+})
