5c6b4b1245bccf5416176c38dab34fef7991edbf
dattt-cy <victomblack1602@gmail.com>
2025-07-11 21:55:27 +0700

    feat: add PostLayout component with responsive grid layout

---
+++ src/layouts/PostLayout/index.tsx
@@ -0,0 +1,39 @@
+'use client'
+import PersistentScrollView from '@/components/common/PersistentScrollView'
+import useResponsive from '@/hooks/useResponsive'
+import { Divider, Grid, Stack } from '@mui/material'
+import { Box } from '@mui/system'
+import React, { ReactNode } from 'react'
+
+interface PostLayoutProps {
+  children: ReactNode
+}
+
+const PostLayout = ({ children }: PostLayoutProps) => {
+  const isMobile = useResponsive('down', 'sm')
+  const isDesktop = useResponsive('up', 'md')
+  return (
+    <Box sx={{ display: 'flex', height: '100%' }}>
+      <Grid
+        sx={{
+          width: isMobile ? '100%' : '80%',
+          padding: isMobile ? '12px' : '0',
+          borderRadius: '10px'
+        }}
+      >
+        <PersistentScrollView id='scrollableDiv'>{children}</PersistentScrollView>
+      </Grid>
+      <Divider orientation='vertical' flexItem sx={{ color: 'rgba(204.44, 128.17, 240.32, 0.25)' }} />
+      <Grid
+        sx={{
+          display: { xs: 'none', sm: 'none', md: 'block' },
+          width: isMobile ? '0' : '32%',
+          padding: '0 22px',
+          borderRadius: '10px'
+        }}
+      ></Grid>
+    </Box>
+  )
+}
+
+export default PostLayout
